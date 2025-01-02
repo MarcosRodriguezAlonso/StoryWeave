cfcfad4aa8c12fe37e8770d54169a45ffbdbf4a3
Marcos <e.marcosra@gmail.com>
2025-01-02 04:08:50 +0100

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenidx a StoryWeave</h1>
+      <p>Aquí comienzxa tu historia</p>
+    </div>
+  );
+};
+
+export default Home;
